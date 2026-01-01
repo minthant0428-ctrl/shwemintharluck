d8657086f0e4020214193ffd00a97e439cd26c88
myo <minthant0428@gmail.com>
2026-01-01 20:24:21 +0630

    Add files via upload

---
+++ ShweMinThar/script.js
@@ -0,0 +1,5 @@
+function joinNow() {
+    alert("Casino Website သို့ ဝင်ရောက်နေပါသည်...");
+    // Affiliate link ထည့်ချင်ရင် ဒီနေရာမှာ URL ပြောင်းနိုင်
+    // window.open("https://your-affiliate-link.com", "_blank");
+}
